db7f360503465b5d692dad889379da32f943b69a
vagrant <vagrant@localhost.localdomain>
2016-03-09 17:47:38 +0000

    variables en php

--- prueba1.php
+++ prueba1.php
@@ -1,5 +1,41 @@
 <?php
+//comentario una linea
 
-echo "Hola universo";
+/*
+varias
+lineas
+*/
+
+print('Hola que hace ');
+echo "Hola universo \n";
+
+
+//tipos variables
+
+$boolean = true; //TRUE or true
+$bollean = false; //FALSE or false
+
+$int1 = 12;
+$int2 = -12;
+$int3 = 012; //octal
+$int4 = 0x0F; //hexadecimal
+
+
+$float = 1.234;
+$float2 = 1.2e3; //1200
+$float3 = 7E-10;
+
+unset($init1); //quitar el valor de la variable
+ var_dump($float);
+ var_dump($int4);
+
+ $sum = 1 + 1;
+ $res = 2 - 1.0;
+ $mul = 6*5;
+ $div = 6/2;
+
+ var_dump($res);
+ var_dump($div);
+
 
 ?>
